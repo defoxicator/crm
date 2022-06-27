e9586d9d14c3e6fda2fe07f3fed60102ab8aa565
defoxicator <wsadwasd0@gmail.com>
2022-06-27 08:22:14 +0200

    added signals

--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,6 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import Group
 
 from accounts.decorators import unauthenticated_user
 
