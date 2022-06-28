db89cf946995011863620bbad15204b99a2e36cf
defoxicator <104618176+defoxicator@users.noreply.github.com>
2022-06-28 18:42:07 +0200

    heroku error no 8

--- crm/settings.py
+++ crm/settings.py
@@ -155,6 +155,11 @@ EMAIL_HOST_PASSWORD = 'p4ssword'
 
 # S3 buckets config
 
+AWS_ACCESS_KEY_ID = 'AKIA25HETXAJRKXSNYXE'
+AWS_S3_REGION_NAME = 'eu-central-1'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'defoxicator-crm-bucket'
+
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
