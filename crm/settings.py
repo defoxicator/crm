ceb5ecc9068efc48879c13367d352e015d43a1ee
defoxicator <104618176+defoxicator@users.noreply.github.com>
2022-06-28 18:17:36 +0200

    heroku error no 7

--- crm/settings.py
+++ crm/settings.py
@@ -155,7 +155,6 @@ EMAIL_HOST_PASSWORD = 'p4ssword'
 
 # S3 buckets config
 
-AWS_S3_REGION_NAME = 'eu-central-1'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
