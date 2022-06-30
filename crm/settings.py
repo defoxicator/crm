0b24a0fe0326b650bf6021915a0c9c18e2968f10
defoxicator <wsadwasd0@gmail.com>
2022-06-30 11:07:13 +0200

    aws that is, NOT working!

--- crm/settings.py
+++ crm/settings.py
@@ -182,7 +182,7 @@ AWS_S3_REGION_NAME = os.environ['AWS_S3_REGION_NAME']
 
 STATIC_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
 AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = 'public-read'
+AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
