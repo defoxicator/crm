96731c6c70516c38c93d7836ae7965d7006c6b9f
defoxicator <wsadwasd0@gmail.com>
2022-06-30 09:04:32 +0200

    if not aws mabye cloudinary

--- crm/settings.py
+++ crm/settings.py
@@ -165,8 +165,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 #SMTP Configuration
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
