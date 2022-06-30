c0f9e961f981e16db441c41ec2554facaaf706e0
defoxicator <wsadwasd0@gmail.com>
2022-06-30 10:54:10 +0200

    aws that is, NOT working!

--- crm/settings.py
+++ crm/settings.py
@@ -187,3 +187,5 @@ AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+## REPAIR CUSTOMER GROUP ALIGMENT
+## REPAIR S3 AGAIN!
