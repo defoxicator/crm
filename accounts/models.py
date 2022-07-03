ecea74f9e25c784568ffe8678a408b4f18b8ae43
defoxicator <104618176+defoxicator@users.noreply.github.com>
2022-07-03 15:14:22 +0200

    admin can access backend

--- accounts/models.py
+++ accounts/models.py
@@ -8,7 +8,7 @@ class Customer(models.Model):
     name = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=200, null=True, blank=True)
     email = models.CharField(max_length=200, null=True, blank=True)
-    profile_pic = models.ImageField(default="def_pic.jpg", null=True)
+    profile_pic = models.ImageField(default="profile.jpg", null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
